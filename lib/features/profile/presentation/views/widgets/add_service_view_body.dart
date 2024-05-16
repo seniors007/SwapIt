2e7ea807c281a0e875473659f402a077011c6ef2
David Amir <seniros2002class@gmail.com>
2024-05-16 17:50:44 +0300

    fetch categories from Api

--- lib/features/profile/presentation/views/widgets/add_service_view_body.dart
+++ lib/features/profile/presentation/views/widgets/add_service_view_body.dart
@@ -1,5 +1,6 @@
-import 'package:dio/dio.dart';
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 import 'package:swapit/core/utils/constants.dart';
 import 'package:swapit/core/widgets/custom_text_field.dart';
 
@@ -11,133 +12,117 @@ class AddServiceViewBody extends StatefulWidget {
 }
 
 class _AddServiceViewBodyState extends State<AddServiceViewBody> {
-  String _dropdownvalue = 'Category';
-  Dio dio = Dio();
+  String _dropdownValue = '';
+  List<Map<String, dynamic>> _categories = []; // Store categories here
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCategories();
+  }
+
+  Future<void> fetchCategories() async {
+    try {
+      Response response =
+          await Dio().get('http://localhost:5204/api/categories/getall');
+      if (response.statusCode == 200) {
+        setState(() {
+          _categories = List<Map<String, dynamic>>.from(response.data);
+          _dropdownValue =
+              _categories.isNotEmpty ? _categories[0]['id'].toString() : '';
+        });
+      } else {
+        throw Exception('Failed to fetch categories');
+      }
+    } catch (e) {
+      log('Error fetching categories: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SingleChildScrollView(
-        physics: const ClampingScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            child: Column(
-              children: [
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      height: 180,
-                    ),
-                    Text(
-                      textAlign: TextAlign.left,
-                      'Add Service',
-                      style: TextStyle(
-                          color: kGreenColor,
-                          fontSize: 25,
-                          fontWeight: FontWeight.w500),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Row(
-                  children: [
-                    Text(
-                      textAlign: TextAlign.left,
-                      'Service Category',
-                      style: TextStyle(
-                          color: kGreenColor,
-                          fontSize: 17,
-                          fontWeight: FontWeight.w500),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                DropdownButton(
-                  items: const [
-                    DropdownMenuItem(
-                      value: 'Decoration',
-                      child: Text('Decoration'),
-                    ),
-                    DropdownMenuItem(
-                      value: 'Elictrical',
-                      child: Text('Elictrical'),
-                    ),
-                    DropdownMenuItem(
-                      value: 'Cleaning',
-                      child: Text('Cleaning'),
-                    ),
-                  ],
-                  value: _dropdownvalue,
-                  onChanged: dropdownCallback,
-                  iconSize: 50,
-                  isExpanded: true,
-                  iconDisabledColor: kYellowColor,
-                  iconEnabledColor: kGreenColor,
-                ),
-                const SizedBox(
-                  height: 15.0,
-                ),
-                const CustomTextField(label: 'Service Discription'),
-                const SizedBox(
-                  height: 15.0,
-                ),
-                const CustomTextField(label: 'Price'),
-                const SizedBox(
-                  height: 15.0,
-                ),
-                const CustomTextField(label: 'Postion'),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    // try {
-                    //   Response response = await dio.get(
-                    //       'http://127.0.0.1:5204/api/users/IsUserNameExists?username=bbbbbbb');
-                    //   if (response.statusCode == 200) {
-                    //     print(response.data);
-                    //   } else {
-                    //     print(
-                    //         'Request failed with status: ${response.statusCode}');
-                    //   }
-                    // } catch (e) {
-                    //   print('Error: $e');
-                    // }
-                    Navigator.pop(context);
-                  },
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: kYellowColor),
-                  child: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      'Add',
-                      style: TextStyle(
-                        color: kWhiteColor,
-                        fontSize: 20,
+      child: Center(
+        child: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              child: Column(
+                children: [
+                  const Text(
+                    textAlign: TextAlign.left,
+                    'Add Service',
+                    style: TextStyle(
+                        color: kGreenColor,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  DropdownButtonFormField<String>(
+                    items:
+                        _categories.map<DropdownMenuItem<String>>((category) {
+                      return DropdownMenuItem<String>(
+                        value: category['id'].toString(),
+                        child: Text(category['name']),
+                      );
+                    }).toList(),
+                    value: _dropdownValue,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _dropdownValue = newValue!;
+                      });
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Service Category',
+                      labelStyle: TextStyle(color: kGreenColor),
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kGreenColor),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  const CustomTextField(label: 'Service Description'),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  const CustomTextField(label: 'Price'),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  const CustomTextField(label: 'Position'),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      log(_dropdownValue);
+                      Navigator.pop(context);
+                    },
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: kYellowColor),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Add',
+                        style: TextStyle(
+                          color: kWhiteColor,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
-  }
-
-  void dropdownCallback(String? selectedValue) {
-    if (selectedValue is String) {
-      setState(() {
-        _dropdownvalue = selectedValue;
-      });
-    }
   }
 }
